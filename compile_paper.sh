4436828d27077d8c4b6b79e9034045a0afb0a9de
Richard Smith-Unna <richardsmith404@gmail.com>
2015-10-09 18:41:02 +0100

    Cleanup

--- compile_paper.sh
+++
@@ -1,9 +0,0 @@
-# generate figures
-/code/produce_figures.R
-
-# compile markdown to PDF
-pandoc -s -S \
---biblio references/references.bib \
---csl chicago-author-date.csl \
-paper.md \
--o paper.pdf
